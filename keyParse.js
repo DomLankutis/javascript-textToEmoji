16807a96f06c417d5fe7a78b9f94effe338306f8
Domantas Lankutis <domantas.lankutis@ada.ac.uk>
2018-05-21 19:45:17 +0100

    Added numbers, cleaned up the code

--- keyParse.js
+++ keyParse.js
@@ -4,34 +4,11 @@ let emojiBet = {};
  * Use this function in the preload to load all the sprites without causing any issues
  */
 function loadKeySprites() {
-    emojiBet = {
-        a: loadImage("./keys/a.png"),
-        b: loadImage("./keys/b.png"),
-        c: loadImage("./keys/c.png"),
-        d: loadImage("./keys/d.png"),
-        e: loadImage("./keys/e.png"),
-        f: loadImage("./keys/f.png"),
-        g: loadImage("./keys/g.png"),
-        h: loadImage("./keys/h.png"),
-        i: loadImage("./keys/i.png"),
-        j: loadImage("./keys/j.png"),
-        k: loadImage("./keys/k.png"),
-        l: loadImage("./keys/l.png"),
-        m: loadImage("./keys/m.png"),
-        n: loadImage("./keys/n.png"),
-        o: loadImage("./keys/o.png"),
-        p: loadImage("./keys/p.png"),
-        q: loadImage("./keys/q.png"),
-        r: loadImage("./keys/r.png"),
-        s: loadImage("./keys/s.png"),
-        t: loadImage("./keys/t.png"),
-        u: loadImage("./keys/u.png"),
-        v: loadImage("./keys/v.png"),
-        w: loadImage("./keys/w.png"),
-        x: loadImage("./keys/x.png"),
-        y: loadImage("./keys/y.png"),
-        z: loadImage("./keys/z.png")
-    };
+    let charactersToLoad = "abcdefghijklmnopqrstuvwxyz1234567890";
+    for (let i = 0; i < charactersToLoad.length; i++) {
+        let char = charactersToLoad.charAt(i);
+        emojiBet[char] = loadImage("./keys/"+char+".png");
+    }
 }
 
 /**
@@ -44,7 +21,7 @@ function loadKeySprites() {
  */
 function printEmojiText(text, startX, startY, font = 50) {
     for (let i = 0; i < text.length; i++){
-        if (text.charAt(i) != " "){
+        if (text.charAt(i) != " " && text.charAt(i) != "_"){
             image(emojiBet[text.charAt(i).toLowerCase()], startX + (i * font), startY, font, font);
         }
     }
